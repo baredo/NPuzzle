d0ae9a0c347e75849569d6d9f3bbdf7b042b174e
Francisco <franciscojbh1993@gmail.com>
2017-08-17 12:56:52 +0200

    Puesta a punto de github

---
+++ n-puzzle/aaaa/Action.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace n_puzzle {
+    class Action {
+        //string name;
+        //short id;
+        int cost;
+
+        public string name { get; }
+        public short id { get; }
+
+        public Action(string name, short id, int cost=1) {
+            this.name = name;
+            this.cost = cost;
+            this.id = id;
+        }
+
+        public bool isPossible(State state) {
+
+            return false;
+        }
+    }
+}
---
+++ n-puzzle/aaaa/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace n_puzzle {
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+
+    enum Direction : short { up, down, left, right }
+    public partial class MainWindow : Window {
+
+        public MainWindow() {
+            InitializeComponent();
+
+            List<Action> actionList = new List<Action>();
+            actionList.Add(new Action("goUp", (short)Direction.up));
+            actionList.Add(new Action("goDown", (short)Direction.down));
+            actionList.Add(new Action("goLeft", (short)Direction.left));
+            actionList.Add(new Action("goRight", (short)Direction.right));
+
+            State initState = new State(3, 3, actionList);
+            State goalState = new State(3, 3, actionList);
+            goalState.setGoal();
+
+            Func<State, List<Action>> convertMethod = actionAllowed;
+
+            State newState = initState.doAction(actionList.ElementAt(0));
+            int b = 3;
+
+        }
+
+        private static List<Action> actionAllowed(State state) {
+            return null;
+        }
+
+    }
+}
