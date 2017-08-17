bea63ad7304ad315784eb35089234f3984b9d54b
Usuario <Usuario@PRACTICAS>
2017-08-17 12:48:26 +0200

    Agregar archivos de proyecto.

---
+++ n-puzzle/Tree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace n_puzzle {
+    class Tree {
+        Queue<Node> queue;
+        public Tree(State state) {
+            Node node = new Node(state, null, null, 0);
+        }
+
+    }
+}
